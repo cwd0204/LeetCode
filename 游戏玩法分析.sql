507ad5cab718c931845edbba933389c4585b1e4d
cwd0204 <cwd0204@126.com>
2021-01-26 16:29:14 +0800

    d
    d

--- 游戏玩法分析.sql
+++ 游戏玩法分析.sql
@@ -71,3 +71,12 @@ Result table:
 | 3         | 1         |
 +-----------+-----------+
 
+SELECT player_id,
+       device_id
+FROM activity
+WHERE (player_id,
+       event_date) IN
+    (SELECT player_id,
+            min(event_date)
+     FROM activity
+     GROUP BY player_id)
